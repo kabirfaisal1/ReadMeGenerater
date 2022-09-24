f41c1e3bcb872b772b622d1d469d52d93226a04f
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-09-24 12:45:40 -0400

    added git ignore and node

--- index.js
+++ index.js
@@ -1,13 +1,129 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const file_system = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of this project? (Required)',
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('Please enter your title!');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'Deployed_URL',
+    message: 'What is the Deployed URL (Required)',
+    validate: deployedURLInput => {
+        if (deployedURLInput) {
+            return true;
+        } else {
+            console.log('Please enter Deployed URL!');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'gitRepo',
+    message: 'What is the gitRepo (Required)',
+    validate: gitRepoInput => {
+        if (gitRepoInput) {
+            return true;
+        } else {
+            console.log('Please enter gitRepo');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'User_Story',
+    message: 'What is the User Story',
+    validate: UserStory_Input => {
+        if (UserStory_Input) {
+            return true;
+        } else {
+            console.log('Please enter gitRepo');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'Acceptance_Criteria',
+    message: 'What is the Acceptance Criteria',
+    validate: AcceptanceCriteria_Input => {
+        if (AcceptanceCriteria_Input) {
+            return true;
+        } else {
+            console.log('Please enter Acceptance Criteria');
+            return false;
+        }
+    }
+},
+{
+    type: 'Checkbox',
+    name: 'dependency',
+    message: 'What are the Dependency (Required)',
+    choices: ['node.js', 'nmp', 'ASP.Net', 'PHP'],
+    validate: Dependency_Input => {
+        if (Dependency_Input) {
+            return true;
+        } else {
+            console.log('Please enter Dependency');
+            return false;
+        }
+    }
+},
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+var writeToFile = file_output=> {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./output//generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
 
 // Function call to initialize app
-init();
+init().then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license !== 'no license') {
+        return `
+      ![badge](https://img.shields.io/badge/license-${license}-blue)
+        `;
+      } else {
+        return ' ';
+      }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license)  {
+    if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+  }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(confirmContributers, data) {
+    if (!confirmContributers) {
+      return `
+    Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+      `;
+    } else {
+      return `
+    ${data}
+      `;
+    }
+  }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
-}
+    return `
+    # ${data.title}
+    
+    ${renderLicenseBadge(data.license)}
+    ## Table-of-Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseTOC(data.license)}
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## [Description](#table-of-contents)
+    ${data.Deployed_URL}
+    ${data.gitRepo}
+    ${data.User_Story}
+    ${data.Acceptance_Criteria}
+
+    ## [Installation](#table-of-contents)
+    ${data.dependency}
+ 
+    
+    For more information on how to add screenshots for examples, visit the following website:
+    
+    [Mark Down Tutorial](https://agea.github.io/tutorial.md/)
+    
+    ${renderLicenseSection(data.license)}
+    ## [Contributing](#table-of-contents)
+    
+    ${renderContributingSection(data.confirmContributers, data.contribute)}
+    ## [Tests](#table-of-contents)
+    ${data.test}
+    ## [Questions](#table-of-contents)
+    Please contact me using the following links:
+    [GitHub](https://github.com/${data.githubUsername})
+    [Email: ${data.email}](mailto:${data.email})
+  `;
+  }
 
 module.exports = generateMarkdown;
